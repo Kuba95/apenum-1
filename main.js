5b39a62894c1b0a23e1f27c0b317173b94d78fa4
Jakub Szkaradek <kubas15051995@gmail.com>
2018-06-06 20:20:11 +0200

    first

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    addScrolled();
+});
+$(window).scroll(function () {
+    addScrolled();
+});
+
+function addScrolled() {
+    let navHeight = $('#main-nav').outerHeight();
+    let actualPos = $(window).scrollTop();
+    if (actualPos > navHeight) {
+        $('#main-nav').addClass('scrolled');
+    }else{
+          $('#main-nav').removeClass('scrolled');
+    }
+
+}
+
+
+$('#carouselExampleIndicators').carousel({
+    'interval':4000,
+});
